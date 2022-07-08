837fa0fe746ec16728be6c8ac17d997c999021b1
NicoApollo <chinho2007@hotmail.com>
2022-07-08 17:45:19 -0300

    crg

--- Javascript/Cadastro.js
+++ Javascript/Cadastro.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form');
 const email = document.getElementById('email');
 const nome = document.getElementById("nome");
 const senha = document.getElementById('senha');
-const ConfirmaSenha = document.getElementById('confirma_senha');
+const confirmaSenha = document.getElementById('senha1');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -11,13 +11,15 @@ form.addEventListener('submit', (e) => {
 });
 
 function checkInputs() {
-    const emailValue = email.value;
     const nomeValue = nome.value;
+    const emailValue = email.value;
     const senhaValue = senha.value;
-    const ConfirmaSenhaValue = ConfirmaSenha.value;
+    const confirmaSenhaValue = confirmaSenha.value;
 
     if (nomeValue == "") {
-        setErrorFor(nome, "O nome é obrigatório.");
+        setErrorFor(nome, "O nome de usuario é obrigatório.");
+    } else {
+        setSuccessFor(nome);
     }
 }
 
@@ -27,11 +29,11 @@ function setErrorFor(input, message) {
 
     small.innerText = message;
 
-    formControl.className = "formControl error"
+    formControl.className = "form-Control error"
 }
 
 function setSuccessFor(input) {
     const formControl = input.parentElement;
 
-    formControl.className = "formControl success"
+    formControl.className = "form-Control success"
 }
