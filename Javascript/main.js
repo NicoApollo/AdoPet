a20ccc2df4793165ad257c9982817c6350ccec8a
NicoApollo <chinho2007@hotmail.com>
2022-07-26 23:14:24 -0300

    Adicionado visible on\off password

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,1 +1,19 @@
                                         /* Put on/off password */
+function mostrarOcultarSenha() {
+    var senha=document.getElementById("senha");
+    if(senha.type=="password"){
+        senha.type="text";
+    } else{
+        senha.type="password";
+    }
+}
+
+                                        /* Put on/off Check password */
+function mostrarOcultarSenhaDois() {
+    var senha=document.getElementById("confirmasenha");
+    if(senha.type=="password"){
+        senha.type="text";
+    } else{
+        senha.type="password";
+    }
+}
